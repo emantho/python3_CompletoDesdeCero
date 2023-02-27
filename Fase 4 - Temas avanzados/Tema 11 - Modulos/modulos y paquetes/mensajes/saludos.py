f70a19487dfc3b681d366053df699ed8d80370ae
Eder Manjarres <emanja1014@gmail.com>
2023-02-27 16:41:42 -0500

    Iniciado tema 11, creado test.py dentro de folder modulos y paquetes y saludos.py dentro de mensaje dentro de misma carpeta

---
+++ Fase 4 - Temas avanzados/Tema 11 - Modulos/modulos y paquetes/mensajes/saludos.py
@@ -0,0 +1,18 @@
+def saludar():
+    print('Hola te saludo desde saludos.saludar()')
+
+
+class Saludo:
+    def __init__(self):
+        print("Hola, te saludo desde Saludos.__init__()")
+
+
+
+
+
+# para evitar ejecutar el codigo de un modulo desde otro fichero, utilizar una comprobación que devuelve el nombre del fichero mientras se ejecuta:
+
+
+if __name__ == '__main__': # name en este caso contiene el nombre saludar cuando se ejecuta desde otro sitio. Si se ejecuta desde aquí mismo el nombre es main
+    saludar()
+
---
+++ Fase 4 - Temas avanzados/Tema 11 - Modulos/modulos y paquetes/test.py
@@ -0,0 +1,22 @@
+# se esta importando todas las funciones definidad en saludar()
+
+## import saludos
+
+## saludos.saludar()
+
+
+
+# Para importar de forma selectiva se debe cambiar la forma de hacer el import 
+
+# si se tiene mas de una función se añaden usando comas luego del import saludar, otro, otro
+
+## from saludos import saludar
+
+# tambien es posible importar todo usando *
+
+from saludos import * # esto trae todos las definiciones pero carga más la memoria
+
+saludar()
+
+Saludo()
+
